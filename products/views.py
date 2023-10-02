fa816cd61c9be292c6d01697318a4eda5edcebd0
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-10-02 16:27:09 +0100

    Added products to be viewed on website

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """
+    View to return and render the all products on the app,
+    allows the user to sort and search
+    """
+    
+    product = Product.objects.all()
+
+    context = {
+        'products': product
+    }
+
+    return render(request, 'products.html', context)
