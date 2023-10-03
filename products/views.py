fcf4e4980f9208d92ade7dacce06b2616e65439a
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-10-03 09:57:28 +0100

    Add product details view and page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -8,7 +8,7 @@ def all_products(request):
     """
     View to return and render the all products on the app,
     allows the user to sort and search
-    """
+    """ 
     
     product = Product.objects.all()
 
@@ -17,3 +17,19 @@ def all_products(request):
     }
 
     return render(request, 'products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    View each product individually on its own page
+    Passing the individual primary key of the product to the url 
+    Allowing the rendering of that products details on the page
+    """
+    
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'product_detail.html', context)
