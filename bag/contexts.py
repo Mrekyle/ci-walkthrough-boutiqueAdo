653ce3517c0adcce788a48add1833392b1cc3ef8
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-10-14 09:34:35 +0100

    Add total price calculation to context processing

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 """
 By using the separate context file this allows us to use all the context from this file
@@ -12,6 +14,25 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+
+#   Getting the products from the session in the bag variable 
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+         product = get_object_or_404(Product, pk=item_id)
+
+     #     Multiplying the count by the price of the product 
+         total += quantity * product.price
+
+     #     Adding the product count of items in the bag to the quantity variable
+         product_count += quantity
+
+     #     Adding items to the context for use in the bag template view 
+         bag_items.append({
+              'item_id': item_id,
+              'quantity': quantity,
+              'product': product,
+         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
          delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
