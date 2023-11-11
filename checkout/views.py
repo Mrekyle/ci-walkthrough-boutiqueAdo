13afe3070a3c02ab544901519ddd95873e4aab10
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-11-11 18:46:39 +0000

    Added order attatched to profile. Added Users order history. Updated views and templates

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 
 import stripe
 import json
@@ -120,7 +122,33 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            """
+                If the user exists and is logged into the application, then
+                get the following information and pre populate the form with 
+                that information 
+            """
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+                """
+                    If the user doesn't exist or is not logged into the application
+                    render an empty form
+                """
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -142,6 +170,36 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+
+        # Attaching the users profile to the order
+        order.user_profile = profile
+        order.save()
+
+        """
+            If the user has selected to save there information to the profile
+
+            Then get the information from the order and save it into the users profile
+            as long as the form is valid
+        """
+
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address1': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(
+                profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from . models import UserProfile
 from . forms import UserProfileForm
+from checkout.models import Order
 
 # Create your views here.
 
@@ -18,10 +19,10 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Information updated successfully!')
+            messages.success(request, (f'Information updated successfully!'))
         else:
-            messages.error(request, f'Information update failed. Please try again or \
-                           contact us for further support')
+            messages.error(request, (f'Information update failed. Please try again or \
+                           contact us for further support'))
 
     # Getting the users order history and returning all of the previous orders
     orders = profile.orders.all()
@@ -35,6 +36,33 @@ def profile(request):
         'orders': orders,
         # Allows us to avoid displaying certain information when on the profile page
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """
+        Rendering the order history of the users order. When the user
+        clicks on a specific order they will be shown the order confirmation page.
+
+        This allows us to not have to build a separate page to show the same order information.
+
+        Again by using a boolean value in the context, we can check where the user is coming from
+        and render something specific for that instance
+    """
+
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(
+        request, (f'This is a past confirmation for a previous order. '
+                  'A confirmation email was sent of the date the order was placed.'))
+
+    template = 'checkout/checkout_success.html'
+
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
