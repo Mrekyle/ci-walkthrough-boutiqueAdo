b9ee7b6445d38c74074820f5282fa1466393f150
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-11-18 10:21:56 +0000

    Added 404 page

--- botiquqAdo/urls.py
+++ botiquqAdo/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,10 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Ensuring the media location is defined
+
+"""
+    Setting handler404 django knows that is everything fails and something doesnt exist.
+    It should look for this function for what to do. Which then allows a 404 error page to be
+    rendered to the user. 
+"""
+handler404 = 'boutiquqAdo.views.handler404'
